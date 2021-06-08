ce177515d1055265cd93d7673cffac58ebcf8939
passy4ucj <ojinnnakapascal@gmail.com>
2021-06-08 11:13:05 +0100

    Project Initialization

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+
+
+//Load env vars
+dotenv.config({ path: './config/config.env' })
+
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+// Body Parser
+app.use(express.json())
+
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    })
+})
+
+//Use error Middleware
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, 
+        console.log(`Server running on ${process.env.NODE_ENV} mode in PORT ${process.env.PORT}`.yellow.bold))

9261199c40339ea197f35c167d78cd72e9415b71
passy4ucj <ojinnnakapascal@gmail.com>
2021-06-08 11:37:41 +0100

    Redirect URL

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { redirectUrl } = require('../controllers/redirect')
 const { createUrl } = require('../controllers/url')
 
 
@@ -6,5 +7,8 @@ const router = express.Router()
 
 router.route('/shorten')
     .post(createUrl)
+
+router.route('/:code')
+    .get(redirectUrl)
 
 module.exports = router
